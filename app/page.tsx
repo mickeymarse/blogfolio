556e4dcc0bd57643b3146cc858d1ccef062ed458
mickeymarse <mmserrapica@tutanota.com>
2023-12-04 22:20:47 +0000

    Pushed down the wip sign and changed metadata title.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <header className='border border-pink-300 mx-auto max-w-md p-4'>
-      <h1>Work in progress...</h1>
-      <p className='text-pink-100'>Deployment is a first step.</p>
+    <header className='border border-pink-300 mx-auto max-w-md p-4 mt-10'>
+      <h1 className='text-4xl'>Work in progress...</h1>
+      <h6 className='text-pink-100'>Deployment is a first step.</h6>
     </header>
   );
 }
