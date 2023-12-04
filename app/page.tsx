efe2e44107db74546abfd0ec03e21d7796afdb49
mickeymarse <mmserrapica@tutanota.com>
2023-12-04 22:14:52 +0000

    Added a bit of styling to the wip sign

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <h1>Work in progress!!!</h1>;
+  return (
+    <header className='border border-pink-300 mx-auto max-w-md p-4'>
+      <h1>Work in progress...</h1>
+      <p className='text-pink-100'>Deployment is a first step.</p>
+    </header>
+  );
 }
