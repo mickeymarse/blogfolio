132b37ad12497508824ed407d1bd2f5a8554a388
mickeymarse <mmserrapica@tutanota.com>
2024-01-05 17:19:59 +0000

    initial commit

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <header className='border border-pink-300 mx-auto max-w-md p-4 mt-10'>
-      <h1 className='text-4xl'>Work in progress...</h1>
-      <h6 className='text-pink-100'>Deployment is a first step.</h6>
-    </header>
-  );
-}
