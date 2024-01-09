755b07c7125c1fc575fa378c7240f1c4668fb626
mickeymarse <mmserrapica@tutanota.com>
2024-01-09 10:01:32 +0000

    Started styling TypedIntro to test its look.

--- src/components/TypedIntro.tsx
+++ src/components/TypedIntro.tsx
@@ -6,12 +6,22 @@ export default function TypedIntro() {
       options={{
         autoStart: true,
         loop: true,
+        cursor: '_',
+        delay: 150,
+        cursorClassName: 'text-malach text-6xl motion-safe:animate-ping',
+        wrapperClassName: 'text-malach text-6xl',
       }}
       onInit={(typewriter) => {
         typewriter
-          .typeString('<strong>JS</strong> plugin for a cool typewriter effect and')
-          .pauseFor(2500)
-          .deleteAll()
+          .typeString('Welcome!')
+          .pauseFor(3000)
+          .deleteAll(25)
+          .typeString(`I'm Michele Maria Serrapica`)
+          .pauseFor(3000)
+          .changeDeleteSpeed(25)
+          .deleteChars(23)
+          .typeString(`a Full Stack Developer`)
+          .pauseFor(3000)
           .start();
       }}
     />
