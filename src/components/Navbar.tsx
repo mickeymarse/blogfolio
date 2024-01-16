f8527a8f764126cb8ace676f9b9eb33b9de24e28
mickeymarse <mmserrapica@tutanota.com>
2024-01-16 21:55:34 +0000

    Added footer to layout and did a little fix for the navbar.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 export default function Navbar() {
   const [isScrolled, setIsScrolled] = useState(false);
-  const navlink = 'rounded border-4 border-transparent hover:bg-neonp hover:text-back';
+  const navlink = 'rounded border-4 border-transparent hover:bg-malach hover:text-back';
 
   useEffect(() => {
     console.log('useEffect called');
@@ -22,8 +22,8 @@ export default function Navbar() {
     // TODO: Add icons to toggle canvas and dark mode
     <nav
       id='navbar'
-      className={`sticky top-0 left-0 mt-5 ml-5 w-1/3 flex justify-between font-matrix font-bold text-2xl text-white ${
-        isScrolled ? 'bg-back' : 'bg-transparent'
+      className={`sticky top-0 left-0 mt-5 ml-5 w-1/3 flex justify-between font-matrix font-bold text-2xl text-white z-50 ${
+        isScrolled ? 'bg-back rounded' : 'bg-transparent'
       }`}
     >
       <a href='/' className={`${navlink} text-malach motion-safe:animate-pulse`}>
