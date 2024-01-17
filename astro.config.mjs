3d963eb6cb479614c4e89e91e59bcf5aac7e328c
mickeymarse <mmserrapica@tutanota.com>
2024-01-17 15:33:07 +0000

    Removed react-icons. Added astro-icon. Refactored footer using Icon component.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), tailwind()],
+  integrations: [react(), mdx(), tailwind(), icon()]
 });
