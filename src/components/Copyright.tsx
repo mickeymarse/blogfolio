2fad12b0ba070d0eff3effead7827a4a84644072
mickeymarse <mmserrapica@tutanota.com>
2024-01-18 12:44:43 +0000

    Added year and copyright to footer a titles to some of the icons.

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,18 @@
+import { Fragment, useState, useEffect } from 'react';
+
+const Copyright = () => {
+  const [year, setYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    // Update the year when the component mounts
+    setYear(new Date().getFullYear());
+  }, []);
+
+  return (
+    <Fragment>
+      <p>{`© ${year} All Rights Reserved.`}</p>
+    </Fragment>
+  );
+};
+
+export default Copyright;
