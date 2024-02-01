04c9760a41541c2fd25f56b144dcbc69479e760d
mickeymarse <mmserrapica@tutanota.com>
2024-02-01 17:29:41 +0000

    Created Sidebar component and tested Tailwind responsive utilities.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -23,7 +23,7 @@ export default function Navbar({ homeIcon }: any) {
     // TODO: Add icons to toggle canvas and dark mode
     <nav
       id='navbar'
-      className={`sticky top-0 left-0 px-5 py-1 w-full flex justify-between font-sans font-bold text-2xl  z-50 ${
+      className={`sticky top-0 left-0 px-5 py-1 w-full hidden md:flex justify-between font-sans font-bold text-2xl  z-50 ${
         isScrolled ? 'bg-back2 rounded h-20 text-4xl' : 'bg-transparent'
       }`}
     >
