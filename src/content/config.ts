4efd4bfc4e92e8149d9ccb261f7dfa00b1dee6f1
mickeymarse <mmserrapica@tutanota.com>
2024-02-22 16:08:06 +0000

    Created RSS feed.

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,10 @@ import { defineCollection, z } from 'astro:content';
 
 const blog = defineCollection({
   type: 'content',
-  // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
     description: z.string(),
     author: z.string().default('Mickey'),
-    // Transform string to Date object
     pubDate: z.date(),
     heroImage: z.object({
       src: z.string(),
---
+++ src/pages/feed.xml.js
@@ -0,0 +1,29 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    title: `Mickey’s Blog`,
+    description: 'A journal on my journey into tech as a transfem career-changer.',
+    // Pull in your project "site" from the endpoint context
+    site: context.site,
+    // Array of `<item>`s in output xml
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      author: post.data.author,
+      heroImage: post.data.heroImage,
+      // Compute RSS link from post `slug`
+      link: `/blog/${post.slug}/`,
+      content: sanitizeHtml(parser.render(post.body)),
+      ...post.data,
+    })),
+    customData: `<language>en-gb</language>`,
+    stylesheet: '/rss/pretty-feed-v3.xsl',
+  });
+}
