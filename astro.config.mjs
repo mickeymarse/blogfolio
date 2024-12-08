8349c4214e58a4dff94a068882c03b5e1e0d305c
mickeymarse <mmserrapica@tutanota.com>
2024-12-08 14:43:56 +0000

    made the whole repo lighter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
-import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), mdx(), tailwind(), icon()],
+  integrations: [tailwind()],
   site: 'https://www.mickeymarse.dev/',
   trailingSlash: 'never',
 });
